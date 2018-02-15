f7085e5110e2b6a8491e4bcae757539798acf532
Ryan <rjlafazan@gmail.com>
2018-02-14 20:15:06 -0800

    button effect update

--- assets/javascript/love.js
+++ assets/javascript/love.js
@@ -1,7 +1,9 @@
 function myFunction() {
     var txt;
     if (confirm("Do you like me?")) {
-        txt = '&#x1F60D';
+        txt = '&#x1F60D &#x1F618';
+    } else {
+    	txt = '&#x1F62D'
     }
     document.getElementById("answer").innerHTML = txt;
 };
