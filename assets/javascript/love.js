7d7d7e70c74e5e597ee104ebf219a4644102d8ec
Ryan <rjlafazan@gmail.com>
2018-02-14 21:34:43 -0800

    style adjustments and answer links

--- assets/javascript/love.js
+++ assets/javascript/love.js
@@ -2,8 +2,12 @@ function myFunction() {
     var txt;
     if (confirm("Do you like me?")) {
         txt = '&#x1F60D &#x1F618';
+        window.open("https://www.vivalasvegasweddings.com/themed-lasvegasweddings.htm");
+
     } else {
     	txt = '&#x1F62D'
+    	window.open("https://en.wikipedia.org/wiki/Bye,_Felicia");
     }
     document.getElementById("answer").innerHTML = txt;
+    document.getElementById("startbutt").style.opacity = "0"
 };
